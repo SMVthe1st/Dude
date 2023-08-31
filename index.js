633ed39a636cf9f1956a50492310b2f09939f5cf
S. M. V <117322465+SMVthe1st@users.noreply.github.com>
2023-08-31 18:00:35 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,35 @@
+//import AI and readline
+import OpenAI from "openai";
+import readline from "readline";
+
+//create new instance of AI
+const ai = new OpenAI ({
+    organization: "org-ECAN3PQNL9Y6aIax1RrPqWDp",
+    apiKey: "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"
+});
+
+//create interface to send + recieve messages
+const ui = readline.createInterface ({
+    input: process.stdin,
+    output: process.stdout
+});
+
+//user message prompt
+ui.prompt();
+
+//send input
+ui.on("line", async (input) => {
+    //wait for response, log response, prompt for input
+    await ai
+        .chat.completions.create ({
+            model: "gpt-3.5-turbo",
+            messages: [{ "role": "user", "content": input }],
+        })
+        .then ((res) => {
+            console.log(res.choices[0].message.content);
+            ui.prompt();
+        })
+        .catch ((e) => {
+            console.log(e);
+        }); 
+});
